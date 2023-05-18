05d0ce1c0b1cca1df31d040050cfdf52c0c66521
Rodrigo Libraga Fernandes <rodrigo.pagbrasil@pagbrasil.com>
2023-05-18 19:56:29 -0300

    second commit

--- dom.js
+++ dom.js
@@ -1,5 +1,12 @@
 
-$(function(){
-$("header").load("header.html");
-$("footer").load("footer.html");
+
+$(function () {
+  $("header").load("header.html");
+  $("footer").load("footer.html");
 });
+
+$(function () {
+  $("head").load("head.html");
+
+});
+
