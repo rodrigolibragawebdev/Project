2f95b29236be4b95513c0aee9d86a12c5229003f
rodrigolibraga1 <rodrigolibragawebdev@gmail.com>
2023-05-20 20:55:47 -0300

    'first_commit'

--- forms.js
+++ forms.js
@@ -1,19 +1,20 @@
+// // Handle range input
+// const rangeInputs = document.querySelectorAll('input[type="range"]');
 
-// Handle range input
-const rangeInputs = document.querySelectorAll('input[type="range"]')
+// function handleInputChange(e) {
+//   let target = e.target;
+//   if (e.target.type !== "range") {
+//     target = document.getElementById("range");
+//   }
+//   const min = target.min;
+//   const max = target.max;
+//   const val = target.value;
 
-function handleInputChange(e) {
-  let target = e.target
-  if (e.target.type !== 'range') {
-    target = document.getElementById('range')
-  } 
-  const min = target.min
-  const max = target.max
-  const val = target.value
-  
-  target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
-}
+//   target.style.backgroundSize = ((val - min) * 100) / (max - min) + "% 100%";
+// }
 
-rangeInputs.forEach(input => {
-  input.addEventListener('input', handleInputChange)
-})
+// rangeInputs.forEach((input) => {
+//   input.addEventListener("input", handleInputChange);
+// });
+
+//
